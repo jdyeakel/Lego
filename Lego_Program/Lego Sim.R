3a7e94ef9795e1e01d73203ee70117fd6878d931
jdyeakel <jdyeakel@gmail.com>
2014-09-25 15:42:23 -0600

    Fixed new coproduct generator - updating to global resources

--- Lego_Program/Lego Sim.R
+++ Lego_Program/Lego Sim.R
@@ -3,7 +3,10 @@
 #########################
 
 source("rstring.r")
+source("insertRow.r")
+
 str_length <- 10
+max_ab <- 1000
 
 #Initial conditions
 
@@ -13,6 +16,7 @@ a <- numeric()
 b <- list()
 #List of resources
 c <- list()
+c_ab <- list()
 
 #Define eden species
 a[1] <- paste("sp_",rstring(1,str_length),sep="")
@@ -22,6 +26,13 @@ c0_size <- round(runif(1,0,10),0)
 #Draw random initial resources
 c[[1]] <- rstring(c0_size,str_length)
 
+#Initiate Global Resources + abundances
+#The identity of the resource/coproduct/species needs to be paired with numerical abundance
+Rid <- c(c[[1]],a[1])
+Rab <- sample(seq(1,max_ab),length(Rid))
+R <- data.frame(Rid,Rab,row.names=NULL)
+colnames(R) <- c("ID","Abund")
+
 #Build the coproducts
 #What is the probability that a unique coproduct is formed?
 pr_newco <- 1/length(c0)
@@ -29,5 +40,29 @@ b[[1]] <- sample(c[[1]],round(runif(1,0,length(c[[1]])),0))
 #For each coproduct draw a probability of creating a new coproduct
 #Create new coproducts in accordance to this probability
 draw_newco <- runif(length(b[[1]])) < pr_newco
-newco <- rstring(length(which(draw_newco)),str_length)
-b[[1]] <- c(b[[1]],newco)
+num_newco <- length(which(draw_newco))
+if (num_newco > 0) {
+  
+  newco_id <- rstring(num_newco,str_length)
+  newco_ab <- sample(seq(1,max_ab),length(newco_id))
+  #newco <- data.frame(newco_id,newco_ab,row.names=NULL)
+  
+  #Update the coproduct list for the eden organism
+  b[[1]] <- c(b[[1]],newco_id)
+  
+  #Rebuild Global resource matrix
+  Rid <- c(as.character(R$ID),newco_id)
+  Rab <- c(R$Abund,newco_ab)
+  R <- data.frame(Rid,Rab,row.names=NULL)
+  colnames(R) <- c("ID","Abund")
+
+}
+
+#Establish Resource use for the eden organism
+c_ab[[1]] <- 
+
+
+
+
+
+
---
+++ Lego_Program/insertRow.R
@@ -0,0 +1,5 @@
+insertRow <- function(existingDF, newrow, r) {
+  existingDF[seq(r+1,nrow(existingDF)+1),] <- existingDF[seq(r,nrow(existingDF)),]
+  existingDF[r,] <- newrow
+  existingDF
+}
