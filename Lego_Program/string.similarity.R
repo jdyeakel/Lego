f9798429ace7118fe636776720efadf3bf4f30e9
jdyeakel <jdyeakel@gmail.com>
2014-09-30 14:15:11 -0600

    revised string.similarity function into the Jaccard index. Currently the number of resources is not taken into account.

--- Lego_Program/string.similarity.R
+++ Lego_Program/string.similarity.R
@@ -1,23 +1,14 @@
-string.similarity <- function(x,xab,y,yab) {
+string.similarity <- function(x,y) {
   st1 <- x
   st2 <- y
-  
-  st1.ab <- xab
-  st2.ab <- yab
   
   l.st1 <- length(st1)
   l.st2 <- length(st2)
   
-  is.same <- numeric(l.st1)
-  for (i in 1:l.st1) {
-    if (st1[i] %in% st2) {
-      is.same[i] <- 1
-    } else {is.same[i] <- 0}
-  }
-  tot.same <- sum(is.same)
+  str.intersect <- length(intersect(st1,st2))
   
-  overlap <- which(is.same==1)
-  
-  ab.diff <- 
+  Jaccard <- str.intersect / length(unique(c(st1,st2)))
+    
+  return(Jaccard)
   
 }
