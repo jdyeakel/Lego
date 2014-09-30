b2e894b1073d8f8ac78cbf5026587114daeec2a3
jdyeakel <jdyeakel@gmail.com>
2014-09-30 11:56:17 -0600

    Added Resource INUSE dataframe to match R dataframe

--- Lego_Program/Lego Sim.R
+++ Lego_Program/Lego Sim.R
@@ -9,7 +9,9 @@ str.length <- 10
 max.ab <- 1000
 t.term <- 100
 
+####################################
 #Initial conditions (t = 1)
+####################################
 
 #Global properties
 niche.space <- numeric(t.term)
@@ -77,11 +79,122 @@ for (i in 1:length(c[[1]])) {
   c.ab[[1]][i] <- min(rexp(1,rate=0.25),R$Abund[res])
 }
 
+#Create a Resource In Use dataframe
+R.inuse <- R
+tot.res.use <- numeric(length(R$ID))
+for (i in 1:length(R$ID)) {
+  res <- as.character(R$ID[i])
+  #Find which species is using resource i
+  sp.res.use <- unlist(lapply(c,function(x){which(x==res)}))
+  if (length(sp.res.use) > 0) {
+    #Obtain the abundances of resource i in use
+    sp.res.use.ab <- numeric(length(a))
+    for (j in 1:length(a)) {
+      sp.res.use.ab[j] <- c.ab[[j]][sp.res.use[j]]
+    }
+    tot.res.use[i] <- sum(sp.res.use.ab)
+  } else {tot.res.use[i] <- 0}
+}
+R.inuse$Abund <- tot.res.use
+
 
 #Calculate Global Properties of the System
 niche.space[1] <- length(R$ID)
 sp.richness[1] <- length(a)
 avg.complexity[1] <- mean(unlist(lapply(c,length)))
+
+
+
+
+
+####################################
+#Forward Iterations
+####################################
+
+
+#Dynamic properties of the system
+
+
+
+
+#Begin time iterations
+for (t in 2:t.term) {
+  
+  #First, allow mutations for each species
+  #Mutation rate
+  m.rate <- 0.01
+  m.draw <- runif(length(a),0,1) < m.rate
+  mut.event <- which(m.draw)
+  
+  #Initiate mutations ~ new species with small differences in respective b (coproduct) and c (resource) vectors
+  if (length(mut.event)>0) {
+    
+    #The IDs of mut.event determine which species are mutating
+    new.sp <- numeric(length(mut.event))
+    mut.res <- list(length(mut.event))
+    
+    for (i in 1:length(mut.event)) {
+      mutated <- mut.event[i]
+      new.sp[i] <- paste("sp.",rstring(i,str.length),sep="")
+      #Resources should be similar to resources of mutated species
+      old.res <- c[[mutated]]
+      
+      #Build new resource list. Should be similar, but not the same.
+      #For now, we can just delete one resource and replace it with a random draw from the R list
+      mut.res[[i]] <- old.res[-sample(seq(1,length(old.res)),1)]
+      
+      #Ensure that the new resource is not already in it's list
+      new.res <- mut.res[[i]][1]
+      while (new.res %in% mut.res[[i]] == TRUE) {
+        new.res <- as.character(sample(R$ID,1))
+      }
+      mut.res[[i]] <- c(mut.res[[i]],new.res)
+      
+    }
+    
+  }
+  
+  
+  
+  
+  #Determine species similarities
+  for (i in 1:length(a)) {
+    
+  }
+  
+  #Set dynamic rates
+  
+  
+  #Extinction rate
+  #For each species, what is the degree of competition?
+  
+  #For each species, what is the degree of predation?
+  
+  ext.rate <- 
+  
+  
+  
+  
+  
+  #For each species, determine whether a mutation occurs
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
