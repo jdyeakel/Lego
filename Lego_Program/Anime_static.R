e02784ad3342d9f05b14850e112fef0461c9855c
MathiasPires <mathiasmpires@gmail.com>
2015-06-14 11:38:20 -0300

    Changes 1. Pairwise probabilities computed from marginal probs (easier to set probs) obs: shortest.paths is not working

--- Lego_Program/Anime_static.R
+++ Lego_Program/Anime_static.R
@@ -1,6 +1,6 @@
 
 rm(list=c(ls()))
-
+setwd("/Users/piresmm/git/Lego/Lego_Program/")
 library(igraph)
 library(plotrix)
 library(RColorBrewer)
@@ -14,37 +14,67 @@ source("R/build_template.R")
 
 tic <- 0
 
-i=100
+i<-50 #template size
 #for (i in sequence) {
   
   tic <- tic + 1
   
   num_play <- i
   
-  pw_prob <- c(
-    pr_ne = 0.025,
-    pr_nn = 0.025,
-    pr_ni = 0.05,
-    pr_nm = 0.005,
-    pr_ia = 0.05,
-    pr_ie = 0.2,
-    pr_ii = 0.5,
-    pr_aa = 0.05,
-    pr_ee = 0.05
-  )
+#   pw_prob <- c(
+#     pr_ne = 0.025,
+#     pr_nn = 0.025,
+#     pr_ni = 0.05,
+#     pr_nm = 0.005,
+#     pr_ia = 0.05,
+#     pr_ie = 0.2,
+#     pr_ii = 0.5,
+#     pr_aa = 0.05,
+#     pr_ee = 0.05
+#   )
+
+#Defining probabilities of each type
+#Basic probs could also be based on num_play. e.g., We should expect p.n*num_play n's per column/row
+  
+    p.n=0.02 
+    p.e=0.1
+    p.i=0.72
+    p.m=0.1
+    p.a=0
+
+#Normalization [0,1]
+S_prob=sum(c(p.n,p.e,p.i,p.m,p.a))
+p.n=p.n/S_prob
+p.e=p.e/S_prob
+p.i=p.i/S_prob
+p.m=p.m/S_prob
+p.a=p.a/S_prob
+
+#Defining paiwise probabilities 
+pw_prob <- c(
+    pr_ne = p.n*(p.e/(p.e+p.n+p.i+p.m)),
+    pr_nn = p.n*(p.n/(p.e+p.n+p.i+p.m)),
+    pr_ni = p.n*(p.i/(p.e+p.n+p.i+p.m)),
+    pr_nm = p.n*(p.m/(p.e+p.n+p.i+p.m)),
+    pr_ia = p.i*(p.a/(p.e+p.a+p.n+p.i)),
+    pr_ie = p.i*(p.e/(p.e+p.a+p.n+p.i)),
+    pr_ii = p.i*(p.i/(p.e+p.a+p.n+p.i)),
+    pr_aa = p.a*(p.a/(p.a+p.i)),
+    pr_ee = p.e*(p.e/(p.i+p.n+p.e))
+   )
+
+
   #make sure this vector sums to one
   pw_prob <- pw_prob / sum(pw_prob)
   
   #Build the interaction template
-  int_m <- build_template(num_play,pw_prob)
-  
-  
+  int_m <- build_template(num_play,pw_prob, 0.8)
   
   #Some funky things
   # Passive players can 'make' things... this might be okay (chemical rxns)
   
   #Proportion of active players
-  #player_id <- apply(int_m,1,function(x){length(which(x == "e")) > 0})*1
+  player_id <- apply(int_m,1,function(x){length(which(x == "e")) > 0})*1
   #prop_active[tic] <- sum(player_id)/num_play
   
   #fw_connectance[tic] <- 
@@ -60,6 +90,7 @@ i=100
 
 #Visualize matrix:
 xx=matrix(as.numeric(as.factor(int_m)),c(num_play,num_play))
+par(mar=c(1,1,1,4))
 color2D.matplot(xx,extremes=c(1:5), border="white", axes=FALSE, xlab="", ylab="",main="")
 legend(i,i,legend=levels(as.factor(int_m)),pch=22,pt.bg=c(1:5),xpd=TRUE, bty="n")
 
@@ -72,16 +103,20 @@ basal_pos <- 1
 trophic <- sapply(1:num_play,function(x){shortest.paths(fw_g,v = basal_pos, to = x)})
 trophic[which(trophic==Inf)] <- 0
 coords <- cbind(runif(num_play),trophic); coords[basal_pos,] <- c(0.5,trophic[basal_pos])
+par(mar=c(1,1,1,1))
 plot(fw_g,layout=coords,vertex.size=5,edge.arrow.size=0.4,
      main=ecount(fw_g)/num_play^2,vertex.label=NA,
      vertex.color=pal[player_id+1])
 
 ###############################
 
+#Compatibility of subsampled community
 
 #for 'need' coexistence: if vector of need for spA != player vector, spA is locally extinct
 
-
+source("R/test_compatability.R")
+labels <- sort(sample(1:num_play,30))
+test_compatability (int_m,labels)
 
 
 
