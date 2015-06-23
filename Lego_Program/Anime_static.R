6e161899c44c7ef78d307582c8bc46a604299d67
jdyeakel <jdyeakel@gmail.com>
2015-06-23 13:22:08 -0600

    reverted test_compatability function to mathias' version

--- Lego_Program/Anime_static.R
+++ Lego_Program/Anime_static.R
@@ -1,6 +1,6 @@
 
 rm(list=c(ls()))
-setwd("/Users/piresmm/git/Lego/Lego_Program/")
+#setwd("/Users/piresmm/git/Lego/Lego_Program/")
 library(igraph)
 library(plotrix)
 library(RColorBrewer)
@@ -16,11 +16,11 @@ tic <- 0
 
 i<-50 #template size
 #for (i in sequence) {
-  
+
 tic <- tic + 1
 
 num_play <- i
-  
+
 #   pw_prob <- c(
 #     pr_ne = 0.025,
 #     pr_nn = 0.025,
@@ -35,12 +35,12 @@ num_play <- i
 
 #Defining probabilities of each type
 #Basic probs could also be based on num_play. e.g., We should expect p.n*num_play n's per column/row
-  
-    p.n=0.02 
-    p.e=0.1
-    p.i=0.72
-    p.m=0.1
-    p.a=0
+
+p.n=0.02 
+p.e=0.1
+p.i=0.72
+p.m=0.1
+p.a=0
 
 #Normalization [0,1]
 S_prob=sum(c(p.n,p.e,p.i,p.m,p.a))
@@ -52,34 +52,34 @@ p.a=p.a/S_prob
 
 #Defining paiwise probabilities 
 pw_prob <- c(
-    pr_ne = p.n*(p.e/(p.e+p.n+p.i+p.m)),
-    pr_nn = p.n*(p.n/(p.e+p.n+p.i+p.m)),
-    pr_ni = p.n*(p.i/(p.e+p.n+p.i+p.m)),
-    pr_nm = p.n*(p.m/(p.e+p.n+p.i+p.m)),
-    pr_ia = p.i*(p.a/(p.e+p.a+p.n+p.i)),
-    pr_ie = p.i*(p.e/(p.e+p.a+p.n+p.i)),
-    pr_ii = p.i*(p.i/(p.e+p.a+p.n+p.i)),
-    pr_aa = p.a*(p.a/(p.a+p.i)),
-    pr_ee = p.e*(p.e/(p.i+p.n+p.e))
-   )
+  pr_ne = p.n*(p.e/(p.e+p.n+p.i+p.m)),
+  pr_nn = p.n*(p.n/(p.e+p.n+p.i+p.m)),
+  pr_ni = p.n*(p.i/(p.e+p.n+p.i+p.m)),
+  pr_nm = p.n*(p.m/(p.e+p.n+p.i+p.m)),
+  pr_ia = p.i*(p.a/(p.e+p.a+p.n+p.i)),
+  pr_ie = p.i*(p.e/(p.e+p.a+p.n+p.i)),
+  pr_ii = p.i*(p.i/(p.e+p.a+p.n+p.i)),
+  pr_aa = p.a*(p.a/(p.a+p.i)),
+  pr_ee = p.e*(p.e/(p.i+p.n+p.e))
+)
 
 
-  #make sure this vector sums to one
-  pw_prob <- pw_prob / sum(pw_prob)
-  
-  #Build the interaction template
-  int_m <- build_template(num_play,pw_prob, 0.8)
-  
-  #Some funky things
-  # Passive players can 'make' things... this might be okay (chemical rxns)
-  
-  #Proportion of active players
-  player_id <- apply(int_m,1,function(x){length(which(x == "e")) > 0})*1
-  #prop_active[tic] <- sum(player_id)/num_play
-  
-  #fw_connectance[tic] <- 
-  
-  
+#make sure this vector sums to one
+pw_prob <- pw_prob / sum(pw_prob)
+
+#Build the interaction template
+int_m <- build_template(num_play,pw_prob, 0.8)
+
+#Some funky things
+# Passive players can 'make' things... this might be okay (chemical rxns)
+
+#Proportion of active players
+player_id <- apply(int_m,1,function(x){length(which(x == "e")) > 0})*1
+#prop_active[tic] <- sum(player_id)/num_play
+
+#fw_connectance[tic] <- 
+
+
 #}
 
 #plot(sequence,prop_active,xlab="size",ylab="proportion active players",ylim=c(0,1))
@@ -100,7 +100,7 @@ fw <- (int_m == "e")*1
 fw <- fw[-which(player_id==0),-which(player_id==0)]
 fw_g <- graph.adjacency(fw)
 basal_pos <- 1
-trophic <- sapply(1:num_play,function(x){shortest.paths(fw_g,v = basal_pos, to = x)})
+trophic <- sapply(1:vcount(fw_g),function(x){shortest.paths(fw_g,v = basal_pos, to = x)})
 trophic[which(trophic==Inf)] <- 0
 coords <- cbind(runif(num_play),trophic); coords[basal_pos,] <- c(0.5,trophic[basal_pos])
 par(mar=c(1,1,1,1))
