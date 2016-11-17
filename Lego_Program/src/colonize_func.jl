9c0a0630574738cd16ed90745a8f66ae43109696
jdyeakel <jdyeakel@gmail.com>
2016-11-17 05:25:24 -0500

    intcom added to colonization and extinction module up front but not integrated in any meaningful way

--- Lego_Program/src/colonize_func.jl
+++ Lego_Program/src/colonize_func.jl
@@ -14,17 +14,20 @@ function colonize_func(int_m,tp_m,tind_m,mp_m,mind_m,a_thresh,n_thresh,cid,c_m,c
   crev_mold = copy(crev_m);
   
   # Make a list of species-only in community
-  sponly = copy(cid);
-  dl = Array{Int64}(0);
-  for i=1:length(cid)
-    if int_m[sponly[i],sponly[i]] == 'i'
-      push!(dl,i);
-    end
-  end
-  deleteat!(sponly,dl);
-  lsp = length(sponly);
-  spcid = copy(sponly);
-  
+  intcom = int_m[cid,cid];
+  spcid = find(x->x=='n',diag(intcom));
+  # 
+  # sponly = copy(cid);
+  # dl = Array{Int64}(0);
+  # for i=1:length(cid)
+  #   if int_m[sponly[i],sponly[i]] == 'i'
+  #     push!(dl,i);
+  #   end
+  # end
+  # deleteat!(sponly,dl);
+  # lsp = length(sponly);
+  # spcid = copy(sponly);
+  # 
   
   #List of primary producers
   prim_prod = find(x->x=='a',int_m[:,1]);
