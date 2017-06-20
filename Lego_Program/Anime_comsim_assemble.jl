60117eac070c103ace30d5a848145ddb1e26ebc5
jdyeakel <jdyeakel@gmail.com>
2017-06-20 09:27:07 -0600

    some random junk

--- Lego_Program/Anime_comsim_assemble.jl
+++ Lego_Program/Anime_comsim_assemble.jl
@@ -70,28 +70,6 @@ save(namespace,"potcol",potcol,"numsp",numsp,"trophic",trophic);
 # quit()
 
 
-namespace = "$(homedir())/Dropbox/PostDoc/2014_Lego/Lego_Program/figures/fig_potcol_comb.pdf"
-R"""
-library(RColorBrewer)
-cols = brewer.pal(length($varvec),'Set1')
-for (i in 1:length(cols)) {cols[i] = paste(cols[i],'70',sep='')}
-pdf($namespace,width=12,height=6);
-par(mfrow=c(1,2));
-pcol = apply($(potcol[1]),1,mean);
-pcolsd = apply($(potcol[1]),1,sd);
-plot(seq(1,$tmax)/$(numsp[1][1]),pcol/$(numsp[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.25),xlab='Proportion filled',ylab='Proportion potential colonizers',col=cols[1],lwd=3)
-polygon(x=c(seq(1,$tmax)/$(numsp[1][1]),rev(seq(1,$tmax)/$(numsp[1][1]))),y=c((pcol-pcolsd)/$(numsp[1][1]),rev((pcol+pcolsd)/$(numsp[1][1]))),col=cols[1],border=NA)
-legend(x=0.8,y=0.25,legend=$varvec,col=cols,pch=16,title='pr(m)' ,cex=1,bty='n');
-"""
-for i=2:length(varvec)
-  R"""
-  pcol = apply($(potcol[i]),1,mean);
-  pcolsd = apply($(potcol[i]),1,sd);
-  ns = mean($(numsp[i]));
-  lines(seq(1,$tmax)/$(numsp[1][1]),pcol/$(numsp[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.3),col=cols[$i],lwd=3)
-  polygon(x=c(seq(1,$tmax)/ns,rev(seq(1,$tmax)/ns)),y=c((pcol-pcolsd)/ns,rev((pcol+pcolsd)/ns)),col=cols[$i],border=NA)
-  """
-end
 # diff1 = Array{Int64}(0);
 # diff2 = Array{Int64}(0);
 # for r=1:reps
@@ -191,50 +169,74 @@ save(namespace,"potcol",potcol,"numsp",numsp,"trophic",trophic);
 # quit()
 
 
+
+###Plotting###
+
+
+
+varvec_pm = [0.0001 0.001 0.002];
+varvec_nt = [0.2 0.3 0.4];
+tmax = 1000;
+
+dpm = load("$(homedir())/Dropbox/PostDoc/2014_Lego/Lego_Program/data/assemble_pm.jld");
+potcol_pm = dpm["potcol"];
+numsp_pm = dpm["numsp"];
+trophic_pm = dpm["trophic"];
+dnt = load("$(homedir())/Dropbox/PostDoc/2014_Lego/Lego_Program/data/assemble_nt.jld");
+potcol_nt = dnt["potcol"];
+numsp_nt = dnt["numsp"];
+trophic_nt = dnt["trophic"];
+
+
+namespace = "$(homedir())/Dropbox/PostDoc/2014_Lego/Lego_Program/figures/fig_potcol_comb.pdf"
+R"""
+library(RColorBrewer)
+cols = brewer.pal(length($varvec_pm),'Set1')
+for (i in 1:length(cols)) {cols[i] = paste(cols[i],'70',sep='')}
+#pdf($namespace,width=12,height=6);
+par(mfrow=c(1,2));
+pcol = apply($(potcol_pm[1]),1,mean);
+pcolsd = apply($(potcol_pm[1]),1,sd);
+plot(seq(1,$tmax)/$(numsp_pm[1][1]),pcol/$(numsp_pm[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.25),xlab='Proportion filled',ylab='Proportion potential colonizers',col=cols[1],lwd=3)
+polygon(x=c(seq(1,$tmax)/$(numsp_pm[1][1]),rev(seq(1,$tmax)/$(numsp_pm[1][1]))),y=c((pcol-pcolsd)/$(numsp_pm[1][1]),rev((pcol+pcolsd)/$(numsp_pm[1][1]))),col=cols[1],border=NA)
+legend(x=0.8,y=0.25,legend=$varvec_pm,col=cols,pch=16,title='pr(m)' ,cex=1,bty='n');
+"""
+for i=2:length(varvec_pm)
+  R"""
+  pcol = apply($(potcol_pm[i]),1,mean);
+  pcolsd = apply($(potcol_pm[i]),1,sd);
+  ns = mean($(numsp_pm[i]));
+  lines(seq(1,$tmax)/$(numsp_pm[1][1]),pcol/$(numsp_pm[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.3),col=cols[$i],lwd=3)
+  polygon(x=c(seq(1,$tmax)/ns,rev(seq(1,$tmax)/ns)),y=c((pcol-pcolsd)/ns,rev((pcol+pcolsd)/ns)),col=cols[$i],border=NA)
+  """
+end
+
+
 # namespace = "$(homedir())/Dropbox/PostDoc/2014_Lego/Lego_Program/figures/fig_potcol_nt.pdf"
 R"""
 library(RColorBrewer)
-cols = brewer.pal(length($varvec),'Set1')
+cols = brewer.pal(length($varvec_nt),'Set1')
 #cols[1] = cols[3];
 #cols[2] = cols[4];
 for (i in 1:length(cols)) {cols[i] = paste(cols[i],'70',sep='')}
 #pdf($namespace,width=12,height=6);
 #par(mfrow=c(1,2));
-pcol = apply($(potcol[1]),1,mean);
-pcolsd = apply($(potcol[1]),1,sd);
-plot(seq(1,$tmax)/$(numsp[1][1]),pcol/$(numsp[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.25),xlab='Proportion filled',ylab='Proportion potential colonizers',col=cols[1],lwd=3)
-polygon(x=c(seq(1,$tmax)/$(numsp[1][1]),rev(seq(1,$tmax)/$(numsp[1][1]))),y=c((pcol-pcolsd)/$(numsp[1][1]),rev((pcol+pcolsd)/$(numsp[1][1]))),col=cols[1],border=NA)
-legend(x=0.8,y=0.25,legend=$varvec,col=cols,pch=16,title='n_t' ,cex=1,bty='n');
+pcol = apply($(potcol_nt[1]),1,mean);
+pcolsd = apply($(potcol_nt[1]),1,sd);
+plot(seq(1,$tmax)/$(numsp_nt[1][1]),pcol/$(numsp_nt[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.25),xlab='Proportion filled',ylab='Proportion potential colonizers',col=cols[1],lwd=3)
+polygon(x=c(seq(1,$tmax)/$(numsp_nt[1][1]),rev(seq(1,$tmax)/$(numsp_nt[1][1]))),y=c((pcol-pcolsd)/$(numsp_nt[1][1]),rev((pcol+pcolsd)/$(numsp_nt[1][1]))),col=cols[1],border=NA)
+legend(x=0.8,y=0.25,legend=$varvec_nt,col=cols,pch=16,title='n_t' ,cex=1,bty='n');
 """
-for i=2:length(varvec)
+for i=2:length(varvec_nt)
   R"""
-  pcol = apply($(potcol[i]),1,mean);
-  pcolsd = apply($(potcol[i]),1,sd);
-  ns = mean($(numsp[i]));
-  lines(seq(1,$tmax)/$(numsp[1][1]),pcol/$(numsp[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.3),col=cols[$i],lwd=3)
+  pcol = apply($(potcol_nt[i]),1,mean);
+  pcolsd = apply($(potcol_nt[i]),1,sd);
+  ns = mean($(numsp_nt[i]));
+  lines(seq(1,$tmax)/$(numsp_nt[1][1]),pcol/$(numsp_nt[1][1]),type='l',xlim=c(0,1),ylim=c(0,0.3),col=cols[$i],lwd=3)
   polygon(x=c(seq(1,$tmax)/ns,rev(seq(1,$tmax)/ns)),y=c((pcol-pcolsd)/ns,rev((pcol+pcolsd)/ns)),col=cols[$i],border=NA)
   """
 end
-# diff1 = Array{Int64}(0);
-# diff2 = Array{Int64}(0);
-# for r=1:reps
-#   append!(diff1,diff(potcol[1][(find(x->x!=0,potcol[1][:,r])),r]));
-#   append!(diff2,diff(potcol[2][(find(x->x!=0,potcol[2][:,r])),r]));
-# end
-#
-# R"""
-# hist(c($diff1),xlim=c(0.5,max(c($diff1,$diff2))),ylim=c(0,0.4),col=cols[1],xlab='Niche space expansion',main='',freq=F)
-# """
-# for i=2:length(varvec)
-#   diff2 = Array{Int64}(0);
-#   for r=1:reps
-#     append!(diff2,diff(potcol[i][(find(x->x!=0,potcol[i][:,r])),r]));
-#   end
-#   R"""
-#   hist(c($diff2),xlim=c(0.5,max(c($diff1,$diff2))),col=cols[$i],xlab='Niche space expansion',main='',add=T,freq=F)
-#   """
-# end
-R"dev.off()"
+#R"dev.off()"
 
 
 
