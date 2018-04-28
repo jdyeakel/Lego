69e001140ed1a374d4a2684b8094e4eeeffa4b3b
jdyeakel <jdyeakel@gmail.com>
2018-04-28 09:13:19 -0700

    while loop in assembly_trim

--- Anime/src/assembly_trim.jl
+++ Anime/src/assembly_trim.jl
@@ -22,41 +22,48 @@ function assembly_trim(
     
     CID = Array{Bool}(tmax,S*2)*false;
     
-    for t = 1:tmax
-      # if mod(t,1000)==0
-      #   println(string("t=",t))
-      # end
-      
-      cid_old = copy(cid);
-      
-      colcheck = colonizations[t];
-      extcheck = extinctions[t];
-      
-      cid,
-      lpot_col[t],
-      status[t],
-      prim_ext[t],
-      sec_ext[t] = colext(
-      int_m,a_b,n_b,i_b,m_b,n_b0,sp_v,int_id,
-      cid,a_thresh,n_thresh,colcheck,extcheck);
-      
-      CID[t,cid] = true;
-      
-      # rich[t], sprich[t], turnover[t], res_overlap[t], res_overlap_all, conn[t], conn_ind[t] = dynstructure(cid,cid_old,sp_v,a_b,tp_m,tind_m);      
-      # 
-      # res_overlap_dist[t,1:length(res_overlap_all)] = res_overlap_all;
-      # # spcid = intersect(sp_v,cid);
-      # # spcid_ind = indexin(spcid,[1;sp_v]);
-      # 
-      # #Trophic and degrees at tmax
-      # deg,troph = structure(S,cid,sp_v,tind_m);
-      # degrees[t,1:length(deg)] = deg;
-      # trophic[t,1:length(troph)] = troph;
-      # 
-      # #These are 'potential degrees'
-      # # degrees = sum(a_b[cid,:],2);
-      # avgdegree[t] = mean(degrees[t,1:length(deg)]);
+    maxsize = 0;
+    
+    #This will rerun the assembly process if the community does not assemble >10 species
+    while maxsize < 10
+    
+        for t = 1:tmax
+          # if mod(t,1000)==0
+          #   println(string("t=",t))
+          # end
+          
+          cid_old = copy(cid);
+          
+          colcheck = colonizations[t];
+          extcheck = extinctions[t];
+          
+          cid,
+          lpot_col[t],
+          status[t],
+          prim_ext[t],
+          sec_ext[t] = colext(
+          int_m,a_b,n_b,i_b,m_b,n_b0,sp_v,int_id,
+          cid,a_thresh,n_thresh,colcheck,extcheck);
+          
+          CID[t,cid] = true;
+          
+          # rich[t], sprich[t], turnover[t], res_overlap[t], res_overlap_all, conn[t], conn_ind[t] = dynstructure(cid,cid_old,sp_v,a_b,tp_m,tind_m);      
+          # 
+          # res_overlap_dist[t,1:length(res_overlap_all)] = res_overlap_all;
+          # # spcid = intersect(sp_v,cid);
+          # # spcid_ind = indexin(spcid,[1;sp_v]);
+          # 
+          # #Trophic and degrees at tmax
+          # deg,troph = structure(S,cid,sp_v,tind_m);
+          # degrees[t,1:length(deg)] = deg;
+          # trophic[t,1:length(troph)] = troph;
+          # 
+          # #These are 'potential degrees'
+          # # degrees = sum(a_b[cid,:],2);
+          # avgdegree[t] = mean(degrees[t,1:length(deg)]);
 
+        end
+        maxsize = maximum(sum(CID,2));
     end
     
     return(cid,lpot_col,status,prim_ext,sec_ext,CID)
