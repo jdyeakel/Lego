81e646d46d2602ddc8bedfd27fe147bab7896d2a
jdyeakel <jdyeakel@gmail.com>
2020-07-04 06:29:02 -0700

    just the essentials for the paper

--- Lego_Program/R/insertRow.R
+++
@@ -1,5 +0,0 @@
-insertRow <- function(existingDF, newrow, r) {
-  existingDF[seq(r+1,nrow(existingDF)+1),] <- existingDF[seq(r,nrow(existingDF)),]
-  existingDF[r,] <- newrow
-  existingDF
-}
--- Lego_Program/stop_test.jl
+++
@@ -1,9 +0,0 @@
-for i=1:10
-  print(i)
-end
-
-quit()
-
-for i=11:20
-  print(i)
-end
